dd10f2390373de9f6b7e19b311ea04308e05cdc8
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-10-27 01:41:42 +0500

    lecture 2 complete with basic covered in c++

--- C++ in One Shot/main.cpp
+++ C++ in One Shot/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
 int main() {
-    char grade = 'a';
-    //this conversion which is implicitly done by the compiler
-    int value = grade;
-    std::cout<<value;
+//    char grade = 'a';
+//    //this conversion which is implicitly done by the compiler
+//    int value = grade;
+//    std::cout<<value;
+
+//    double price {100.99};
+//    int newprice {0};
+//    newprice = (int) price;
+//    std::cout<<newprice;
+
+
+//Increment and decrement operators
+//post increment operator
+//    int a {10};
+//
+//    int b {a++};    //pehlay kaam hoga, phir update hogi value b mai...
+//    std::cout<<b<<std::endl;
+//    std::cout<<a<<std::endl;
+
+    //Pre increment OPERATOR
+    int a {10};
+
+    int b {++a};    //pehly update hoga, phir kaam hoga
+    std::cout<<b<<std::endl;
+    std::cout<<a<<std::endl;
+
+
     return 0;
 }
