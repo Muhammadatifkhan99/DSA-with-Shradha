9573d611e8dd0883e5da2d0ad40efc052084c0b8
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-10-26 18:18:12 +0500

    C++ type casting and basics

---
+++ C++ in One Shot/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    char grade = 'a';
+    //this conversion which is implicitly done by the compiler
+    int value = grade;
+    std::cout<<value;
+    return 0;
+}
