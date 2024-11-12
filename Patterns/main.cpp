df52bcb5268b7a28af19f4d39f6786137a3a5e8b
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-12 10:29:58 +0500

    floyd's triangle

--- Patterns/main.cpp
+++ Patterns/main.cpp
@@ -7,19 +7,21 @@ int main() {
     cin>>num;
 
     //CONDITION
-    if (num > 0) {
-        cout << "STAR PATTERN IS HERE" << endl;
-    } else
-        cout<<"Enter an integer greater than 0 for the pattern to be made"<<endl;
+//    if (num > 0) {
+//        cout << "STAR PATTERN IS HERE" << endl;
+//    } else
+//        cout<<"Enter an integer greater than 0 for the pattern to be made"<<endl;
 
-    int number{1};
-    char ch{'A'};
-    for(int i{0};i<num;i++){
-        for (int j = 0; j < i+1; ++j) {
-            cout<<ch<<" ";
-            ch++;
+
+    for(int i{0};i<num;i++) {
+        ///printing spaces
+        for (int j = 0; j < i; ++j) {
+            cout << " ";
         }
-        cout<<endl;
+        for (int j = 0; j < num - i; ++j) {
+            cout << i + 1;
+        }
+        cout << endl;
     }
     return 0;
 }
