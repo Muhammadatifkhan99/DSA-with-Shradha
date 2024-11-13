c55ce466090f7819654f9343c254163641e3e309
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-13 21:33:57 +0500

    sum of digits of a number using functions

--- functions/main.cpp
+++ functions/main.cpp
@@ -2,37 +2,54 @@
 using namespace std;
 
 
-int minOfTwo(int a, int b){
-    if( a > b){
-        return a;
-    }
-    else
-        return b;
-}
+//int minOfTwo(int a, int b){
+//    if( a > b){
+//        return a;
+//    }
+//    else
+//        return b;
+//}
+//
+////function to find the sum of numbers to n
+//
+//int sumOfNumbers(int number){
+//    int sum{0};
+//    for(int i{0};i<=number;i++){
+//        sum += i;
+//    }
+//    return sum;
+//}
+////factorial
+//
+//int factorialN(int n){
+//    int fact{1};
+//    for(int i{1};i<=n;i++){
+//        fact *= i;
+//    }
+//    return fact;
+//}
 
-//function to find the sum of numbers to n
 
-int sumOfNumbers(int number){
-    int sum{0};
-    for(int i{0};i<=number;i++){
-        sum += i;
+//sumofdigits of a number
+int sumOfNumber(int num){
+    int digitSum {0};
+    while (num > 0){
+        int lastDigit = {0};
+        lastDigit = num % 10;
+        num = num / 10;
+        digitSum += lastDigit;
     }
-    return sum;
-}
-//factorial
-
-int factorialN(int n){
-    int fact{1};
-    for(int i{1};i<=n;i++){
-        fact *= i;
-    }
-    return fact;
+    return digitSum;
 }
 
 int main() {
-    int a {5};
-    int b {4};
-    cout<<factorialN(a)<<endl;
-    cout<<factorialN(b);
+    cout<<sumOfNumber(145)<<endl;
+    cout<<sumOfNumber(150)<<endl;
+    cout<<sumOfNumber(101)<<endl;
+    cout<<sumOfNumber(001)<<endl;
+    cout<<sumOfNumber(025)<<endl;
+    cout<<sumOfNumber(000)<<endl;
+
+
     return 0;
 }
