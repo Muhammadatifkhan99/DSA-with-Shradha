5febe2dfb861ced538f30d5ba123d4b91a2c0093
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-13 22:35:57 +0500

    calculating the binomial coefficient using functions

--- functions/main.cpp
+++ functions/main.cpp
@@ -19,37 +19,42 @@ using namespace std;
 //    }
 //    return sum;
 //}
-////factorial
-//
-//int factorialN(int n){
-//    int fact{1};
-//    for(int i{1};i<=n;i++){
-//        fact *= i;
-//    }
-//    return fact;
-//}
+//factorial
+
+int factorial(int n){
+    int fact{1};
+    for(int i{1};i<=n;i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+int nCr(int n, int r){
+    int nFact = factorial(n);
+    int rFact = factorial(r);
+    int d {0};
+    d = n - r;
+    int nmrFact = factorial(n-r);
+    int nCr{0};
+    return nFact/(rFact*nmrFact);
+}
 
 
 //sumofdigits of a number
-int sumOfNumber(int num){
-    int digitSum {0};
-    while (num > 0){
-        int lastDigit = {0};
-        lastDigit = num % 10;
-        num = num / 10;
-        digitSum += lastDigit;
-    }
-    return digitSum;
-}
+//int sumOfNumber(int num){
+//    int digitSum {0};
+//    while (num > 0){
+//        int lastDigit = {0};
+//        lastDigit = num % 10;
+//        num = num / 10;
+//        digitSum += lastDigit;
+//    }
+//    return digitSum;
+//}
 
 int main() {
-    cout<<sumOfNumber(145)<<endl;
-    cout<<sumOfNumber(150)<<endl;
-    cout<<sumOfNumber(101)<<endl;
-    cout<<sumOfNumber(001)<<endl;
-    cout<<sumOfNumber(025)<<endl;
-    cout<<sumOfNumber(000)<<endl;
-
-
+//    cout<<factorial(4)<<endl;
+    cout<<"nCr for n = 8, r = 2 is: "<<nCr(8,2)<<endl;
+    cout<<"nCr for n = 6, r = 3 is: "<<nCr(6,3)<<endl;
     return 0;
 }
