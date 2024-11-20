9f62a68b4ab5a38f39e8e70e0c1a5b23977a9d75
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-21 00:39:05 +0500

    excercise at the end of lecture 7

--- BitWiseOperators/main.cpp
+++ BitWiseOperators/main.cpp
@@ -3,15 +3,26 @@ using namespace std;
 
 int main() {
 
+    //Reversing a number
+    int n {125};
+    int digit{0};
+    while(n > 0){
+        int rem = n % 10;
+        n /= 10;    ///used to update the variable in the loop and this prevent an endless loop
+        digit = rem;
+        cout<<digit;
+    }
 
 
-    cout<<"Operator Precendance"<<endl;
+//    cout<<"Operator Precendance"<<endl;
+//
+//    cout<<5 - (2 * 6)<<endl;
+//    cout<<((5 - 2) * 6)<<endl;
+//    cout<<4 * 5 % 2<<endl;
+//    cout<<(4 * (5 % 2))<<endl;
+//    cout<<(5 % 2)<<endl;
 
-    cout<<5 - (2 * 6)<<endl;
-    cout<<((5 - 2) * 6)<<endl;
-    cout<<4 * 5 % 2<<endl;
-    cout<<(4 * (5 % 2))<<endl;
-    cout<<(5 % 2)<<endl;
+ //   cout<<sizeof(long long int)<<endl;
 
 
 
