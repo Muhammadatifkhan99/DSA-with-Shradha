d79410fb6df4989f7e6a0b1440dcb421d19427d9
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-20 23:39:51 +0500

    Bitwise Operators

---
+++ BitWiseOperators/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout<<"The BitWise & is: "<<(6 & 10)<<endl;
+    cout<<"The BitWise | is: "<<(6 | 10)<<endl;
+    cout<<"The BitWise ^ is: "<<(6 ^ 10)<<endl;
+    cout<<"The BitWise << is: "<<(10 << 2)<<endl;
+    cout<<"The BitWise >> is:"<<(10 >> 1)<<endl;
+
+    return 0;
+}
