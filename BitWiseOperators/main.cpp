64d1edc87513d2d2d13bbe6d9f384d5dcb9e8ef7
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-20 23:50:04 +0500

    operator precedance in c++

--- BitWiseOperators/main.cpp
+++ BitWiseOperators/main.cpp
@@ -3,11 +3,25 @@ using namespace std;
 
 int main() {
 
-    cout<<"The BitWise & is: "<<(6 & 10)<<endl;
-    cout<<"The BitWise | is: "<<(6 | 10)<<endl;
-    cout<<"The BitWise ^ is: "<<(6 ^ 10)<<endl;
-    cout<<"The BitWise << is: "<<(10 << 2)<<endl;
-    cout<<"The BitWise >> is:"<<(10 >> 1)<<endl;
+
+
+    cout<<"Operator Precendance"<<endl;
+
+    cout<<5 - (2 * 6)<<endl;
+    cout<<((5 - 2) * 6)<<endl;
+    cout<<4 * 5 % 2<<endl;
+    cout<<(4 * (5 % 2))<<endl;
+    cout<<(5 % 2)<<endl;
+
+
+
+
+
+//    cout<<"The BitWise & is: "<<(6 & 10)<<endl;
+//    cout<<"The BitWise | is: "<<(6 | 10)<<endl;
+//    cout<<"The BitWise ^ is: "<<(6 ^ 10)<<endl;
+//    cout<<"The BitWise << is: "<<(10 << 2)<<endl;
+//    cout<<"The BitWise >> is:"<<(10 >> 1)<<endl;
 
     return 0;
 }
