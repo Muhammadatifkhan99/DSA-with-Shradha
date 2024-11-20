8cc4bc13137521aefaec4065b2abdaa83c4a5497
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-21 01:07:10 +0500

    power of 2 numbers

--- BitWiseOperators/main.cpp
+++ BitWiseOperators/main.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void powerOfTwo(int n){
+    if(n > 0 && (n & (n - 1 )) == 0){
+        cout<<n<<" is power of 2"<<endl;
+    } else {
+        cout<<n<<" is not power of 2"<<endl;
+    }
+}
+
 int main() {
 
+    powerOfTwo(2);
+    powerOfTwo(4);
+    powerOfTwo(5);
+    powerOfTwo(6);
+    powerOfTwo(7);
+    powerOfTwo(8);
+    powerOfTwo(9);
+    powerOfTwo(10);
+
     //Reversing a number
-    int n {125};
-    int digit{0};
-    while(n > 0){
-        int rem = n % 10;
-        n /= 10;    ///used to update the variable in the loop and this prevent an endless loop
-        digit = rem;
-        cout<<digit;
-    }
+//    int n {125};
+//    int digit{0};
+//    while(n > 0){
+//        int rem = n % 10;
+//        n /= 10;    ///used to update the variable in the loop and this prevent an endless loop
+//        digit = rem;
+//        cout<<digit;
+//    }
 
 
 //    cout<<"Operator Precendance"<<endl;
