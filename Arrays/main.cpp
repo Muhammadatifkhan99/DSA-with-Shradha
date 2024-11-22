0e8aa4d9fe6f255f8b655cea2c213ccd28294dc9
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-22 15:31:06 +0500

    finding the largest and smallest values in the array

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,13 +6,16 @@ int main() {
     int num[] = {12,3,12,45,-10};
 
     int smallest = INT_MAX;
+    int largest = INT_MIN;
     //for loop to iterate over the array
-
-    for(int i{0};i < sizeof(num);i++){
-        if(num[i] < smallest){
-            smallest = num[i];
-        }
+    cout<<"The size of the Array num is: "<<sizeof(num)<<endl;
+    int size = (sizeof(num)/4);
+    cout<<"The length of the array is: "<<size<<endl;
+    for(int i{0};i<size;i++){
+        smallest = min(num[i],smallest);
+        largest = max(num[i],largest);
     }
     cout<<"The smallest value in the Array is: "<<smallest<<endl;
+    cout<<"The largest value in the Array is: "<<largest<<endl;
     return 0;
 }
