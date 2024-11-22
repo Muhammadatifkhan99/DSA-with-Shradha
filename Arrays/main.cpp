802c15f8ae65f812eb61c83f19bd574b672d9433
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-22 18:11:51 +0500

    printing the index of the smallest and largest ints in the array

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -8,14 +8,23 @@ int main() {
     int smallest = INT_MAX;
     int largest = INT_MIN;
     //for loop to iterate over the array
-    cout<<"The size of the Array num is: "<<sizeof(num)<<endl;
+    cout<<"The size of the Array in bytes is: "<<sizeof(num)<<endl;
     int size = (sizeof(num)/4);
     cout<<"The length of the array is: "<<size<<endl;
+    int indexSmallest {0};
+    int indexLargest {0};
     for(int i{0};i<size;i++){
-        smallest = min(num[i],smallest);
-        largest = max(num[i],largest);
+        if(num[i] < smallest){
+            smallest = num [i];
+            indexSmallest = i;
+        } else if (num[i] > largest){
+            largest = num [i];
+            indexLargest = i;
+        }
+//        smallest = min(num[i],smallest);
+//        largest = max(num[i],largest);
     }
-    cout<<"The smallest value in the Array is: "<<smallest<<endl;
-    cout<<"The largest value in the Array is: "<<largest<<endl;
+    cout<<"The smallest value in the Array is: "<<smallest<<" at the index of: "<<indexSmallest<<endl;
+    cout<<"The largest value in the Array is: "<<largest<<" at the index of: "<<indexLargest<<endl;
     return 0;
 }
