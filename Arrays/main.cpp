7560a7937c1f2fcf82d67be97ec835edd7091c8b
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-22 18:55:02 +0500

    pass by reference demonstration

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-    int num[] = {12,3,12,45,-10};
-
-    int smallest = INT_MAX;
-    int largest = INT_MIN;
-    //for loop to iterate over the array
-    cout<<"The size of the Array in bytes is: "<<sizeof(num)<<endl;
-    int size = (sizeof(num)/4);
-    cout<<"The length of the array is: "<<size<<endl;
-    int indexSmallest {0};
-    int indexLargest {0};
-    for(int i{0};i<size;i++){
-        if(num[i] < smallest){
-            smallest = num [i];
-            indexSmallest = i;
-        } else if (num[i] > largest){
-            largest = num [i];
-            indexLargest = i;
-        }
-//        smallest = min(num[i],smallest);
-//        largest = max(num[i],largest);
+void changeArr(int arr[], int size){
+    cout<<"In the changeArr function";
+    for (int i = 0; i <= size; ++i) {
+        arr[i] = 2 *arr[i];
     }
-    cout<<"The smallest value in the Array is: "<<smallest<<" at the index of: "<<indexSmallest<<endl;
-    cout<<"The largest value in the Array is: "<<largest<<" at the index of: "<<indexLargest<<endl;
+}
+
+
+int main() {
+    int arr[] = {2,3,4};
+
+    changeArr(arr,4);
+    cout<<endl;
+    for (int (i) = 0; (i) < 4; ++(i)) {
+        cout<<arr[i]<<" ";
+    }
+
+//    int num[] = {12,3,12,45,-10};
+//
+//    int smallest = INT_MAX;
+//    int largest = INT_MIN;
+//    //for loop to iterate over the array
+//    cout<<"The size of the Array in bytes is: "<<sizeof(num)<<endl;
+//    int size = (sizeof(num)/4);
+//    cout<<"The length of the array is: "<<size<<endl;
+//    int indexSmallest {0};
+//    int indexLargest {0};
+//    for(int i{0};i<size;i++){
+//        if(num[i] < smallest){
+//            smallest = num [i];
+//            indexSmallest = i;
+//        } else if (num[i] > largest){
+//            largest = num [i];
+//            indexLargest = i;
+//        }
+////        smallest = min(num[i],smallest);
+////        largest = max(num[i],largest);
+//    }
+//    cout<<"The smallest value in the Array is: "<<smallest<<" at the index of: "<<indexSmallest<<endl;
+//    cout<<"The largest value in the Array is: "<<largest<<" at the index of: "<<indexLargest<<endl;
     return 0;
 }
