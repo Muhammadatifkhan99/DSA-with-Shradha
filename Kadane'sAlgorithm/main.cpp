51a7c4d762ca85c495284179fff04dd72f9c3b3c
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-28 00:05:31 +0500

    printing the maximum sub array sum using brute force approach

--- Kadane'sAlgorithm/main.cpp
+++ Kadane'sAlgorithm/main.cpp
@@ -9,16 +9,17 @@ int main() {
     //printing the subarray of an array
     int n{5};
     int arr[5] {1,2,3,4,5};
-    for (int st = 0; st < n; ++st) {
-        for(int end = st;end < n; end++){
-            for (int i = st; i <= end ; ++i) {
-                cout<<arr[i];
-            }
-            cout<<" ";
-        }
-        cout<<endl;
 
+    int maxSum = {INT_MIN};
+    for (int st = 0; st < n; st++) {
+        int currSum {0};
+        for(int end = st;end < n; end++){
+            currSum += arr[end];
+            maxSum = max(currSum,maxSum);
+        }
     }
+
+    cout<<"The max sub array sum of the this array is: "<<maxSum<<endl;
 
     return 0;
 }
