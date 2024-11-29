9a78e686d6ff138329a699e7f6842d59a7298cdd
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-29 23:57:17 +0500

    finding the majority element using an optimize solution

--- PairSum/main.cpp
+++ PairSum/main.cpp
@@ -1,80 +1,15 @@
 #include<iostream>
 #include<vector>
+#include <algorithm>
 
 using namespace std;
-
-//some how correct solution
-//vector<int> pairSum(vector<int> nums, int target){
-//    int n = nums.size();
-//    vector<int> ans;
-//    for(int i{0};i<n;i++){
-//        if(nums[i] + nums[i+1] == target){
-//            ans.push_back(i);
-//            ans.push_back(i+1);
-//            return ans;
-//        }
-//    }
-//    return ans;
-//}
-
-vector<int> pairSum(vector<int> nums,int target){
-    int n = nums.size();
-    vector<int> ans;
-    for(int i{0};i<n;i++){
-        for(int j{i+1};j<n;j++){
-            if(nums[i] + nums[j] == target){
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-            }
-        }
-    }
-    return ans;
-}
-
-vector<int> pairSum2(vector<int> nums, int target){
-    int n = nums.size();
-    vector<int> ans;
-    //while loop
-    int i{0},j{n-1};
-    while(i<j){
-        int pairSum = nums[i] + nums[j];
-        if (pairSum > target){
-            j--;
-        } else if (pairSum < target){
-            i++;
-        } else {
-            ans.push_back(i);
-            ans.push_back(j);
-            return ans;
-        }
-    }
-    return ans;
-
-
-
-    //using the FOR LOOP
-//    for (int i {0}, j{n-1}; i < j;) {
-//        int pairSum {0};
-//        pairSum = nums[i] + nums[j];
-//        if(pairSum > target){
-//            j--;
-//        } else if ( pairSum < target){
-//            i++;
-//        } else{
-//            ans.push_back(i);
-//            ans.push_back(j);
-//            return ans;
-//        }
-//    }
-}
 
 
 //Majority Element in an Array
 int majorityElement(int arr[],int size){
     int count {0};
     for(int i{0};i<size;i++){
-        for(int j{1};j<=size;j++){
+        for(int j{0};j<size;j++){
             if (arr[i] == arr[j])
                 count ++;
         }
@@ -83,15 +18,47 @@ int majorityElement(int arr[],int size){
         }
     }
 }
-int main() {
-    int nums[] = {1,2,2,2,2,1,1};
-    int size {6};
 
-    cout<<majorityElement(nums,size);
-//    vector<int> ans = pairSum2(nums,target);
-//    for(int val: ans){
-//        cout<<val<<endl;
-//    }
+int majorityElement(vector<int> nums){
+    int n = nums.size();
+    for(int val: nums){
+        int count{0};
+        for(int el: nums){
+            if(val == el){
+                count++;
+            }
+        }
+        if(count> n/2){
+            return val;
+        }
+    }
+}
+
+//Majority Element: an optimize solution
+
+int majorityElementO(vector<int> &nums){
+    int n = nums.size();
+    sort(nums.begin(),nums.end());
+    int freq {1};
+    int ans = nums[0];
+    for(int i{};i<n;i++){
+        if (nums[i] == nums[i-1]){
+            freq++;
+        } else {
+            freq = 1;
+            ans = nums[i];
+        }
+        if (freq > n/2){
+            return ans;
+        }
+    }
+    return ans;
+}
+int main() {
+    int nums[] = {1,2,2,1,1,1,1};
+    int size {6};
+    vector<int> num = {1,1,2,2,3,3,3,3,3};
+    cout<<majorityElementO(num);
 
     return 0;
 }
