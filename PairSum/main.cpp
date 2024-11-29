d1572e9419acd8c9b89d088fbf14748de9189eee
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-29 22:30:06 +0500

    using while loop to solve the pairSum Challenge

--- PairSum/main.cpp
+++ PairSum/main.cpp
@@ -35,23 +35,42 @@ vector<int> pairSum(vector<int> nums,int target){
 vector<int> pairSum2(vector<int> nums, int target){
     int n = nums.size();
     vector<int> ans;
-    for (int i {0}, j{n-1}; i < j;) {
-        int pairSum {0};
-        pairSum = nums[i] + nums[j];
-        if(pairSum > target){
+    //while loop
+    int i{0},j{n-1};
+    while(i<j){
+        int pairSum = nums[i] + nums[j];
+        if (pairSum > target){
             j--;
-        } else if ( pairSum < target){
+        } else if (pairSum < target){
             i++;
-        } else{
+        } else {
             ans.push_back(i);
             ans.push_back(j);
             return ans;
         }
     }
+    return ans;
+
+
+
+    //using the FOR LOOP
+//    for (int i {0}, j{n-1}; i < j;) {
+//        int pairSum {0};
+//        pairSum = nums[i] + nums[j];
+//        if(pairSum > target){
+//            j--;
+//        } else if ( pairSum < target){
+//            i++;
+//        } else{
+//            ans.push_back(i);
+//            ans.push_back(j);
+//            return ans;
+//        }
+//    }
 }
 int main() {
     vector<int> nums = {2,7,11,15};
-    int target {9};
+    int target {27};
 
     vector<int> ans = pairSum2(nums,target);
     for(int val: ans){
