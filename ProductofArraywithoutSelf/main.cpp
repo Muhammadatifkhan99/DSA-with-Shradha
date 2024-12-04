e06833f1b435ad48c88eae2a55ff92d9f0947ce2
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-12-04 22:00:04 +0500

    product of the array without the self element

--- ProductofArraywithoutSelf/main.cpp
+++ ProductofArraywithoutSelf/main.cpp
@@ -15,13 +15,32 @@ int productofArrayExceptSelf(vector<int> &nums, int index){
     return prod;
 }
 
+vector<int> productofArray(vector<int> nums){
+    int n = nums.size();
+    vector<int> prefix(n,1);
+    prefix[0] = 1;
+    //prefix array
+    for(int i{1};i<n;i++){
+        prefix[i] = prefix[i - 1] * nums[i - 1];
+    }
+    vector<int> suffix (n,1);
+    //suffix array
+    for(int i{n-2};i<n;i--){
+        suffix[i] = suffix[i+1] * nums[i + 1];
+    }
+    //for ans vector
+    vector<int> ans(n,1);
+    for(int i{0};i<n; i++){
+        ans[i] = prefix[i] * suffix[i];
+    }
+    return ans;
+}
+
 int main() {
     vector<int> nums {1,2,3,4};
-    int prod = productofArrayExceptSelf(nums,3);
-    vector<int> ans;
-    ans.push_back(prod);
-    for(int val: ans){
+    for(int val: productofArray(nums)){
         cout<<val<<endl;
     }
+
     return 0;
 }
