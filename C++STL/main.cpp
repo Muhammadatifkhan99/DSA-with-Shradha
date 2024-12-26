32e89e1ec8404a19051196e9033aa767a5e69fd2
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-12-26 11:16:21 +0500

    vectors with all methods, iterators , reverse iterators

--- C++STL/main.cpp
+++ C++STL/main.cpp
@@ -7,6 +7,25 @@ using namespace std;
 int main() {
     vector<int> vec {1,2,3,4,5};
 
+    vector<int>::iterator it;
+    //with iterators we can access the direct memory address of these values
+    //it functions the same as the for each loop or the normal for loop
+    for (it = vec.begin();it != vec.end();it++) {
+        cout << *(it) << endl;
+    }
+
+    vector<int>::reverse_iterator itr;
+    for (itr = vec.rbegin();itr != vec.rend();itr ++) {
+        cout << "printing in reverse fashion: " << *(itr) << endl;
+    }
+
+    cout << endl;
+    cout << "Modern Approach " << endl;
+
+    for (auto it1 = vec.begin();it1 != vec.end(); it1 ++) {
+        cout << *(it1) << endl;
+    }
+
     // vec.erase(vec.begin());
     // vec.erase((vec.begin() + 2));
 
@@ -17,10 +36,10 @@ int main() {
     //adding a value at a specific index
     vec.insert(vec.begin() + 2,100);
 
-    for (int val: vec) {
-        cout << val << " ";
-    }
-    cout<< endl;
+    // for (int val: vec) {
+    //     cout << val << " ";
+    // }
+    // cout<< endl;
 
 
 
