e0538dc4ceaeefb2fdfb4d1d5a652049fb9c95db
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-12-28 17:27:44 +0500

    checking if a string is a palindrome or not

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -4,34 +4,59 @@
 
 using namespace std;
 
+bool isPalindrome(string str) {
+    string rstr;
+    rstr = str;
+    reverse(rstr.begin(),rstr.end());
+    if (rstr == str)
+        return true;
+    else
+        return false;
+}
 
 int main() {
-    string str = "Hello World";
+    string str = "110";
 
-    cout << "Reversing a string using the reverse() function from STL: " << endl;
+    if (isPalindrome(str)) {
+        cout << str << " is a valid palindrome" << endl;
+    } else
+        cout << str << " is not a valid palindrome" << endl;
 
-    reverse(str.begin(),str.end());
-    cout << "String reversed using the reverse function: " << str << endl;
 
-    int start {0};
-    int end = str.length() - 1;
-    while (start <= end) {
-        swap(str[start],str[end]);
-        start ++;
-        end --;
-    }
-    cout << "Reversed String: " << endl;
-    cout << str << endl;
 
-    cout << "///////////////////////////";
 
-    for (int i{0};i<str.length();i++) {
-        if (str[i] == ' ') {
-            cout << " ";
-        } else
-            cout << str[i];
-    }
-    cout << endl;
+
+
+
+
+
+
+    // string str = "Hello World";
+    //
+    // cout << "Reversing a string using the reverse() function from STL: " << endl;
+    //
+    // reverse(str.begin(),str.end());
+    // cout << "String reversed using the reverse function: " << str << endl;
+    //
+    // int start {0};
+    // int end = str.length() - 1;
+    // while (start <= end) {
+    //     swap(str[start],str[end]);
+    //     start ++;
+    //     end --;
+    // }
+    // cout << "Reversed String: " << endl;
+    // cout << str << endl;
+    //
+    // cout << "///////////////////////////";
+    //
+    // for (int i{0};i<str.length();i++) {
+    //     if (str[i] == ' ') {
+    //         cout << " ";
+    //     } else
+    //         cout << str[i];
+    // }
+    // cout << endl;
     // string str1 = ", inshallah very soon ";
     //
     // string str3 = str + str1;
