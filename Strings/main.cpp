39ac6791fcc1a6b287c3c2fa9c2b28f6a758a063
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-12-28 17:20:58 +0500

    strings and the associated functions from STL

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include<iostream>
 
 
@@ -5,39 +6,37 @@ using namespace std;
 
 
 int main() {
-    char str [21];
-    cout<< "enter a char array" << endl;
-    cin.getline(str,21,'.');
-    cout<<str<<endl;
-    for (char val: str) {
-        cout << val ;
+    string str = "Hello World";
+
+    cout << "Reversing a string using the reverse() function from STL: " << endl;
+
+    reverse(str.begin(),str.end());
+    cout << "String reversed using the reverse function: " << str << endl;
+
+    int start {0};
+    int end = str.length() - 1;
+    while (start <= end) {
+        swap(str[start],str[end]);
+        start ++;
+        end --;
+    }
+    cout << "Reversed String: " << endl;
+    cout << str << endl;
+
+    cout << "///////////////////////////";
+
+    for (int i{0};i<str.length();i++) {
+        if (str[i] == ' ') {
+            cout << " ";
+        } else
+            cout << str[i];
     }
     cout << endl;
-
-
-
-
-
-    //taking strings input using cin
-    // char str[100];
-    // char str1[100];
-    // cin >> str;
-    // cout<< str << endl;
-    // cout<< "Taking input using cin.getline: " << endl;
-    // cin.getline(str1,100);
-    // cout<< str1 << endl;
-
-    // char str[5] = {'a','t','i','f','\0'};
-    // //C-Style strings are stored this way
-    // //C-Style strings ends with a null character represented by "\0"
+    // string str1 = ", inshallah very soon ";
     //
-    //
-    // int arr [5] = {1,2,3,4,5};
-    // //couting the name of an array prints the memory address of where its stored in the memory
-    // cout<< arr << endl;
-    // cout<<str<< endl;
-
-
+    // string str3 = str + str1;
+    // cout << (str == str1) << endl;
+    // cout << str3 << endl;
 
     return 0;
 }
