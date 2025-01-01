a9200245662816b47da55dd31c19e99515e32905
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-01 18:27:33 +0500

    reverse a string

---
+++ reversingWordsString/main.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+string reverseString(string s) {
+    reverse(s.begin(),s.end());
+    string ans = " ";
+    for (int i{0};i<s.length();i++) {
+        string word = " ";
+        while (i < s.length() && s[i] != ' ') {
+            word += s[i];
+            i++;
+        }
+        reverse(word.begin(), word.end());
+        if (word.length() > 0) {
+            ans += " " + word;
+        }
+    }
+    return ans.substr(1);
+}
+
+int main() {
+    string s = "the pen";
+    cout << reverseString(s) << endl;
+
+
+    return 0;
+}
