330e2ba1b7ebf93928cb70d2fc158aa94381a5f2
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-09 12:45:06 +0500

    finding the maximum sum of the row

--- 2DArraysC++/main.cpp
+++ 2DArraysC++/main.cpp
@@ -28,19 +28,33 @@ pair<int,int> linearSearchPair(int matrix[][3],int row,int col,int key) {
     return {-1,-1};
 }
 
+int maxRowSum(int matrix[][3],int row, int col) {
+    int maxSum {0};
+    for (int i{0};i < row; i++) {
+        int sum {0};
+        for (int j{0};j < col; j++) {
+            sum += matrix[i][j];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+    }
+    return maxSum;
+}
 
 
 int main() {
 
-    int matrix [4][3] = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
-    int row = 4;
+    int matrix [][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int row = 3;
     int col = 3;
-    int target = 8;
+
+    cout << maxRowSum(matrix,row,col) << endl;
 
     // cout << linearSearch(matrix,row,col,target);
-    pair<int,int> rowandCol = {0,0};
-    rowandCol = linearSearchPair(matrix,row,col,target);
-    cout << rowandCol.first << " " << rowandCol.second << endl;
+    // pair<int,int> rowandCol = {0,0};
+    // rowandCol = linearSearchPair(matrix,row,col,target);
+    // cout << rowandCol.first << " " << rowandCol.second << endl;
 
     // for (int i{0};i<row;i++) {
     //     for (int j{0};j < col;j++) {
