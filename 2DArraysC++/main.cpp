7c9ec9cc183353efe057d45375e4202a16dd0c3b
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-09 13:12:10 +0500

    finding max sum of a col

--- 2DArraysC++/main.cpp
+++ 2DArraysC++/main.cpp
@@ -42,14 +42,29 @@ int maxRowSum(int matrix[][3],int row, int col) {
     return maxSum;
 }
 
+int maxColSum(int matrix[][3],int row, int col) {
+    int maxSum {0};
+    for (int i{0};i < col; i++) {
+        int sum {0};
+        for (int j{0};j < row; j++) {
+            sum += matrix[j][i];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+    }
+    return maxSum;
+}
 
 int main() {
 
-    int matrix [][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int matrix [][3] = {{1,9,3},{4,5,6},{7,8,9}};
     int row = 3;
     int col = 3;
 
     cout << maxRowSum(matrix,row,col) << endl;
+    cout << maxColSum(matrix,row,col) << endl;
+
 
     // cout << linearSearch(matrix,row,col,target);
     // pair<int,int> rowandCol = {0,0};
