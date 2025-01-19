67b3df7f4313a5cc595d265b4f8ec06e38b9c742
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-19 17:24:57 +0500

    finding duplicates values in an array(using LL concepts)

---
+++ FindDuplicateSets/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int findDuplicate(vector<int> arr) {
+    int slow = arr[0], fast = arr[0];
+    do {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+    slow = arr[0];
+    while (slow != fast) {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+    return slow;
+}
+
+int main() {
+    vector<int> nums = {1,3,4,2,2};
+
+    cout << findDuplicate(nums) << endl;
+
+
+    return 0;
+}
