c16b3330506946362b166b365ef000b35b02bf47
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-19 11:02:53 +0500

    Two sum using linear time complexity

--- TwoSum/main.cpp
+++ TwoSum/main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include <unordered_map>
 
 using namespace std;
 
+//brute force approach for solving two sums
 pair<int,int> twoSum (vector<int> &mat, int target) {
     int n = mat.size();
     int sum {0};
@@ -40,19 +42,38 @@ pair<int,int> twoSum1(vector<int> &arr,int target) {
     return {-1,-1};
 }
 
+///using hashing to solve the two sum problem on leetcode
+
+vector<int> twoSums (vector<int> arr,int target) {
+    pmr::unordered_map<int,int> m;
+    vector<int> ans;
+    for (int i{0};i<arr.size();i++) {
+        int first = arr[i];
+        int second = target - first;
+        //checks if the key exists in the array or vector
+        if (m.find(second) != m.end()) {
+            ans.push_back(i);
+            ans.push_back(m[second]);
+            break;
+        }
+        m[first] = i;
+    }
+    return ans;
+}
+
 
 int main() {
     vector<int> v = {5,2,11,7,15};
     int target  = 9;
-    cout << "Solution using the brute force approach" << endl;
-    pair<int,int> p1 =  twoSum(v,target);
-    cout << p1.first << endl;
-    cout << p1.second << endl;
-    cout <<"Better Approach then the brute force" << endl;
-    cout <<"The array after sorting is: " << endl;
-    pair<int,int> p2 =  twoSum1(v,target);
-    cout << p2.first << endl;
-    cout << p2.second << endl;
+    // cout << "Solution using the brute force approach" << endl;
+    // pair<int,int> p1 =  twoSum1(v,target);
+    // cout << p1.first << endl;
+    // cout << p1.second << endl;
+    // cout <<"Better Approach then the brute force" << endl;
+    // cout <<"The array after sorting is: " << endl;
+    for (int val: twoSums(v,target)) {
+        cout << val << endl;
+    }
 
 
 
