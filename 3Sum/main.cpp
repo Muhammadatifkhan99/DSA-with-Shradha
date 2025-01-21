8290a11d84f7613e90db969ec6570c6e50b940e2
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-21 12:37:33 +0500

    finding the 3 sum using binarysearch approach

--- 3Sum/main.cpp
+++ 3Sum/main.cpp
@@ -55,27 +55,91 @@ vector<vector<int>> threeSumhashing(vector<int> &nums) {
 //most optimized approach
 
 // coded on leetcode
+/////
+////
+///
+vector<vector<int>> threeSumBS(vector<int> &nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for i
+            for (int j = i + 1; j < n - 1; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // Skip duplicates for j
+                int findit = -(nums[i] + nums[j]);
+                int start = j + 1;
+                int end = n - 1;
+                while (start <= end) {
+                    int mid = start + (end - start) / 2;
+                    if (nums[mid] == findit) {
+                        ans.push_back({nums[i], nums[j], nums[mid]});
+                        break;
+                    } else if (nums[mid] > findit) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+
+
+
+
+    // vector<vector<int>> ans;
+    // sort(nums.begin(),nums.end());
+    // int n = nums.size();
+    // int i = 0;
+    // int j = n - 1;
+    // while (i < j ) {
+    //     int findit = -nums[i] - nums[j];
+    //     int start = i + 1;
+    //     int end = j - 1;
+    //     while (start < end) {
+    //         int mid = start + (end - start)/2;
+    //         if (nums[mid] == findit) {
+    //             ans.push_back({nums[i],nums[j],findit});
+    //         } else if(findit < nums[mid]){
+    //             end = mid - 1;
+    //         } else {
+    //             start = mid + 1;
+    //         }
+    //     }
+    //     i++,j--;
+    // }
+    // return ans;
 
 
 
 int main() {
     vector<int> nums = {-1,0,1,2,-1,-4};
-    vector<vector<int>> threesum = threeSumbf(nums);
+    vector<vector<int>> threesum = threeSumBS(nums);
 
-    cout << "The triplets whose sum is equal to 0: ";
+    cout <<"3 Sum using binary Search Approach: " << endl;
     for (auto val: threesum) {
-        for (auto el:val) {
+        for (auto el: val) {
             cout << el << " " ;
         }
     }
-    cout << endl;
-    vector<vector<int>> threesumhashing = threeSumbf(nums);
-    cout << "The triplets whose sum is equal to 0: ";
-    for (auto val: threesumhashing) {
-        for (auto el:val) {
-            cout << el << " " ;
-        }
-    }
+
+    // cout << "The triplets whose sum is equal to 0: ";
+    // for (auto val: threesum) {
+    //     for (auto el:val) {
+    //         cout << el << " " ;
+    //     }
+    // }
+    // cout << endl;
+    // vector<vector<int>> threesumhashing = threeSumbf(nums);
+    // cout << "The triplets whose sum is equal to 0: ";
+    // for (auto val: threesumhashing) {
+    //     for (auto el:val) {
+    //         cout << el << " " ;
+    //     }
+    // }
 
 
 
