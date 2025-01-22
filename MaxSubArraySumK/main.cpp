fd58a44a9663e96577ac4829605736ce8a6dee47
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-22 21:49:58 +0500

    optimal approach to find the maxsubarraysum

--- MaxSubArraySumK/main.cpp
+++ MaxSubArraySumK/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <unordered_map>
 #include<vector>
 
 using namespace std;
@@ -17,6 +18,46 @@ int maxSubArraySumK(vector<int> &arr, int tar) {
         }
     }
     return count;
+}
+
+//most optimize solution to find the maxsub array
+
+int subarraySum(vector<int>& arr, int tar) {
+    int n = arr.size();
+    int count = 0;
+    vector<int> prefixSum (n,0);
+
+    prefixSum[0] = arr[0];
+    for(int i{1};i < n; i++){
+        prefixSum[i] = prefixSum[i - 1] + arr[i];
+    }
+
+    unordered_map<int,int> m;
+
+    for(int j{0};j<n;j++){
+        if(prefixSum[j] == tar) count++;
+
+        int val = prefixSum[j] - tar;
+        if(m.find(val) != m.end()){
+            count += m[val];
+        }
+
+        if(m.find(prefixSum[j]) == m.end()){
+            m[prefixSum[j]] =0;
+        }
+        m[prefixSum[j]] ++;
+    }
+    return count;
+
+    // for(int i{0};i<n;i++){
+    //     int sum {0};
+    //     for(int j{i};j<n;j++){
+    //         sum += nums[j];
+    //         if(sum == tar){
+    //             count ++;
+    //         }
+    //     }
+    // }
 }
 
 
