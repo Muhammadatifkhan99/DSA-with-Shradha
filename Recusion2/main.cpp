1788b16c566a8b6c6e4d592100ffb1623f8b5f4e
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-25 12:36:22 +0500

    finding if an array is sorted or not

--- Recusion2/main.cpp
+++ Recusion2/main.cpp
@@ -10,9 +10,24 @@ int fibonacci(int n) {
     return fibonacci(n-1) + fibonacci(n -2);
 }
 
+//check if the array is sorted or not
+
+bool isSorted(vector<int> &arr, int n) {
+    if (n == 1 || n == 0) {
+        return true;
+    }
+    return arr[n - 1] >= arr[n - 2] && isSorted(arr,n-1);
+}
+
+//binary search using recursion
+
+
 int main() {
+    vector<int> arr = {1,2,3,4,5};
     int n = 19;
-    cout << "The fibonacci number for " << n << " is:" << fibonacci(n) << endl;
+
+    cout << "The array is sorted: " << isSorted(arr,arr.size()) << endl;
+    // cout << "The fibonacci number for " << n << " is:" << fibonacci(n) << endl;
 
 
     return 0;
