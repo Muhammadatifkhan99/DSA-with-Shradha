38b155585dcdf8a336fab809cd8b3e9346a4d56f
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-27 10:48:08 +0500

    permutations of an array

---
+++ PermutationsInArray/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void getPerms(vector<int> &nums,int idx,vector<vector<int>> &ans) {
+    if (idx==nums.size()) {
+        ans.push_back(nums);
+        return;
+    }
+    for (int i{0};i<nums.size();i++) {
+        swap(nums[idx],nums[i]);
+        getPerms(nums,idx+1,ans);
+        swap(nums[idx],nums[i]);
+    }
+}
+
+
+
+
+int main() {
+
+
+
+
+
+
+
+
+    return 0;
+}
