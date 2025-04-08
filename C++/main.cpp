0587cbe8c53d63d50787e1281b00ba7220de8363
Muhammad Atif <muhammadatifkhan.99@gamil.com>
2025-04-08 22:05:31 +0500

    refreshing basic syntax in c++ for DSA

--- C++/main.cpp
+++ C++/main.cpp
@@ -3,19 +3,66 @@
 using namespace std;
 
 int main() {
-    int a = 2;
-    int b = 4;
+    // int a = 2;
+    // int b = 4;
+    //
+    // if (a > b) {
+    //     cout << "a is greater then b in this case"<<endl;
+    // } else if (b > a) {
+    //     cout << "b is greater then a in this case" <<endl;
+    // } else {
+    //     cout <<"a and b are equal in this case"<<endl;
+    // }
+    int n = 78;
+    int sum {0};
+    int prod = {0};
+    prod = n  *(n + 1);
+    sum = prod / 2;
+    cout << sum << endl;
 
-    if (a > b) {
-        cout << "a is greater then b in this case"<<endl;
-    } else if (b > a) {
-        cout << "b is greater then a in this case" <<endl;
-    } else {
-        cout <<"a and b are equal in this case"<<endl;
+    //sum of numbers from 1 to n
+    cout <<"\nSum of numbers with out using the formala:"<<endl;
+    int ns = 10;
+    int summ {0};
+    for (int i{0};i<=ns;i++) {
+        summ += i;
+    }
+    cout <<summ<<"\n";
+
+
+    cout << (5 / 2) << endl;
+    cout << (5 /(double)2)<< endl;
+
+
+    //Ternary Operators
+    int a = 12;
+    a >= 0 ? cout <<"positive" : cout <<"negative";
+
+    //LOOPS
+    cout <<"\n";
+    int count = 1;
+    while (count <= 5) {
+        cout <<"Apna College"<<endl;
+        count ++;
     }
 
+    //Sum of odd numbers from 1 to N
+
+    cout<<endl;
+    int uptoN = 10;
+    int sumN {0};
+    for (int i{0};i <= uptoN; i++) {
+        if (i % 2 != 0) {
+            sumN += i;
+        }
+    }
+    cout <<"Sum of odd numbers from 1 to "<<uptoN<<" is: "<< sumN <<endl;
+    cout <<"Sum of numbers from 1 to "<<ns<<" is: " <<summ <<endl;
+
+
+
     //VECTORS
-    cout << "Hello World"<<endl;
+    // cout << "Hello World"<<endl;
     //ARRAYS
 //    float n[3] = {12.3,3.3,0.2};
     // const string s = "atif";
