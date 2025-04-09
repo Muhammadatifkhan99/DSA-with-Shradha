ec9d827d9333b27c4ca29eaac068826b276251dc
Muhammad Atif <muhammadatifkhan.99@gamil.com>
2025-04-09 22:23:28 +0500

    updating functins code

--- functions/main.cpp
+++ functions/main.cpp
@@ -2,71 +2,72 @@
 using namespace std;
 
 
-//int minOfTwo(int a, int b){
-//    if( a > b){
-//        return a;
-//    }
-//    else
-//        return b;
-//}
+int minOfTwo(int a, int b){
+    if( a > b){
+        return a;
+    }
+    else
+        return b;
+}
 //
 ////function to find the sum of numbers to n
-//
-//int sumOfNumbers(int number){
-//    int sum{0};
-//    for(int i{0};i<=number;i++){
-//        sum += i;
-//    }
-//    return sum;
-//}
+
+int sumOfNumbers(int number){
+    int sum{0};
+    for(int i{0};i<=number;i++){
+        sum += i;
+    }
+    return sum;
+}
+
+
 //factorial
 
-//int factorial(int n){
-//    int fact{1};
-//    for(int i{1};i<=n;i++){
-//        fact *= i;
-//    }
-//    return fact;
-//}
-//
-//int nCr(int n, int r){
-//    int nFact = factorial(n);
-//    int rFact = factorial(r);
-//    int d {0};
-//    d = n - r;
-//    int nmrFact = factorial(n-r);
-//    int nCr{0};
-//    return nFact/(rFact*nmrFact);
-//}
+int factorial(int n){
+    int fact{1};
+    for(int i{1};i<=n;i++){
+        fact *= i;
+    }
+    return fact;
+    // int i {0};
+    // while (i<=n) {
+    //     fact = fact * i;
+    //     i++;
+    // }
+}
+
+int nCr(int n, int r){
+    return factorial(n)/(factorial(r)*factorial(n-r));
+}
 
 
-//sumofdigits of a number
-//int sumOfNumber(int num){
-//    int digitSum {0};
-//    while (num > 0){
-//        int lastDigit = {0};
-//        lastDigit = num % 10;
-//        num = num / 10;
-//        digitSum += lastDigit;
-//    }
-//    return digitSum;
-//}
+//sum of digits of a number
+int sumOfNumber(int num){
+    int digitSum {0};
+    while (num > 0){
+        int lastDigit = {0};
+        lastDigit = num % 10;
+        num = num / 10;
+        digitSum += lastDigit;
+    }
+    return digitSum;
+}
 
 //finding the nth fibonacci using iterative method
-//int nthFib(int number){
-//    if (number == 0) return 0;
-//    else if ( number == 1) return 1;
-//
-//    int d = 0, b = 1, fib =0;
-//    for(int i = 2; i<=number; i++){
-//        fib = d + b;
-//        d = b;
-//        b = fib;
-//    }
-//    return b;
-//}
+int nthFib(int number){
+    if (number == 0) return 0;
+    else if ( number == 1) return 1;
 
-void isPrime( int number) {
+    int d = 0, b = 1, fib =0;
+    for(int i = 2; i<=number; i++){
+        fib = d + b;
+        d = b;
+        b = fib;
+    }
+    return b;
+}
+
+void isPrime(int number) {
     //we are not checking for 1 and n
     for(int i{2};i<number;i++){
         if(number % i == 0){
@@ -84,7 +85,7 @@ int decimalToBinary(int decNum) {
     while(decNum > 0){
         int rem = decNum % 2;
         decNum /= 2;
-        ans += (rem * pow);
+        ans += (rem * pow); // ans = ans + (rem * pow)
         pow = pow * 10;
     }
     return ans;
@@ -104,6 +105,11 @@ int binaryToDecimal(int decNum){
 
 int main() {
     int num=110;
+    int n = 8;
+    int r = 2;
+    cout << nCr(n,r) << endl;
+    int fib {4};
+    cout << nthFib(fib) << endl;
     cout<<binaryToDecimal(num);
     return 0;
 }
