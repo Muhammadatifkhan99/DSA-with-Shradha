40b56f12b905ae71ff6480f6415c983eeb41e9d2
Muhammad Atif <muhammadatifkhan.99@gamil.com>
2025-04-11 22:30:51 +0500

    revisiting binary exponentiation to calculate the pow(x,n) efficiently

--- PairSum/main.cpp
+++ PairSum/main.cpp
@@ -5,21 +5,7 @@
 using namespace std;
 
 
-//Majority Element in an Array
-// int majorityElement(int arr[],int size){
-//     int count {0};
-//     for(int i{0};i<size;i++){
-//         for(int j{0};j<size;j++){
-//             if (arr[i] == arr[j])
-//                 count ++;
-//         }
-//         if(count > (size/2)){
-//             return arr[i];
-//         }
-//     }
-// }
-
-// int majorityElement(vector<int> nums){
+// int majorityElementBF(vector<int> nums){
 //     int n = nums.size();
 //     for(int val: nums){
 //         int count{0};
@@ -36,51 +22,53 @@ using namespace std;
 
 // //Majority Element: an optimize solution
 
-// int majorityElementO(vector<int> &nums){
-//     int n = nums.size();
-//     sort(nums.begin(),nums.end());
-//     int freq {1};
-//     int ans = nums[0];
-//     for(int i{};i<n;i++){
-//         if (nums[i] == nums[i-1]){
-//             freq++;
-//         } else {
-//             freq = 1;
-//             ans = nums[i];
-//         }
-//         if (freq > n/2){
-//             return ans;
-//         }
-//     }
-//     return ans;
-// }
+int majorityElementOS(vector<int> &nums){
+    int n = nums.size();
+    sort(nums.begin(),nums.end());
+    int freq {1};
+    int ans = nums[0];
+    for(int i{};i<n;i++){
+        if (nums[i] == nums[i-1]){
+            freq++;
+        } else {
+            freq = 1;
+            ans = nums[i];
+        }
+        if (freq > n/2){
+            return ans;
+        }
+    }
+    return ans;
+}
 
-// //MOORE'S Voting Algorithm
-// int mooreAlgorithm(vector<int> nums){
-//     int freq{0};
-//     int ans{0};
-//     for(int i{0};i<nums.size();i++){
-//         if(freq == 0){
-//             ans = nums[i];
-//         }
-//         if(ans == nums[i]){
-//             freq ++;
-//         }else {
-//             freq --;
-//         }
-//     }
-//     int count {0};
-//     for(int val: nums){
-//         if (val == ans){
-//             count ++;
-//         }
-//     }
-//     if(count > nums.size()/2){
-//         return ans;
-//     }
-//     else
-//         return -1;
-// }
+//MOORE'S Voting Algorithm
+int mooreAlgorithm(vector<int> nums){
+    int freq{0};
+    int ans{0};
+    for(int i{0};i<nums.size();i++){
+        if(freq == 0){
+            ans = nums[i];
+        }
+        if(ans == nums[i]){
+            freq ++;
+        }else {
+            freq --;
+        }
+    }
+
+    //this step ensures that if the answer does not exists in the array then return -1;
+    int count {0};
+    for(int val: nums){
+        if (val == ans){
+            count ++;
+        }
+    }
+    if(count > nums.size()/2){
+        return ans;
+    }
+    else
+        return -1;
+}
 
 // vector<int> pairSum(vector<int> nums, int target){
 //   vector<int> ans;
@@ -120,14 +108,15 @@ vector<int> pairSumTwoPointers(vector<int>& nums,int target){
 }
 
 int main(){
-    // int nums[] = {1,2,2,1,1,1,1};
-    // int size {6};
-    // vector<int> num = {1,2,3,4,5};
+    int nums[] = {1,2,2,1,1,1,1};
+    int size {6};
+    vector<int> nums {1,2,2,1,1};
+    // cout << majorityElementOS(nums) << endl;
 
-    vector<int> nums = {2,7,11,15};
-    int target = 9;
-    vector<int> ans = pairSumTwoPointers(nums,target);
-    cout << ans[0] << " " << ans[1] << endl;
+    // vector<int> nums = {2,7,11,15};
+    // int target = 9;
+    // vector<int> ans = pairSumTwoPointers(nums,target);
+    // cout << ans[0] << " " << ans[1] << endl;
 
 
 //    int target = 7;
--- Pow(x,n)/main.cpp
+++ Pow(x,n)/main.cpp
@@ -9,12 +9,12 @@ double mypow(double x,int n){
     if (x == -1 && n % 2 == 0) return 1.0;
     if (x == -1 && n % 2 != 0) return -1.0;
 
-    int binForm{n};
+    long binForm{n};
     if(n < 0 ){
         x = 1/x;
         binForm = -binForm;
     }
-    int ans{1};
+    double ans{1};
     while(binForm > 0){
         if(binForm % 2 == 1){
             ans = ans * x;
