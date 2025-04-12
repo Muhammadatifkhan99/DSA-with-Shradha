7b6d0b4798e19412c38aad4d7dda9edf17449d69
Muhammad Atif <muhammadatifkhan.99@gamil.com>
2025-04-13 00:29:29 +0500

    rework on product of array without self

--- ProductofArraywithoutSelf/main.cpp
+++ ProductofArraywithoutSelf/main.cpp
@@ -14,7 +14,24 @@ int productofArrayExceptSelf(vector<int> &nums, int index){
     }
     return prod;
 }
+//brute force apporach
+vector<int> productExceptSelf(vector<int>& nums){
+    vector<int> ans(nums.size(), 1);
 
+    for(int i{0};i<nums.size();i++){
+        int prod {1};
+        for(int j{0};j<nums.size();j++){
+            //this condition will skip value at ith index
+            if(i != j){
+                prod *= nums[j];
+            }
+        }
+        ans[i] = prod;
+    }
+    return ans;
+
+}
+//optiml solution but O(n) space complexity 
 vector<int> productofArray(vector<int> nums){
     int n = nums.size();
     vector<int> prefix(n,1);
@@ -36,11 +53,35 @@ vector<int> productofArray(vector<int> nums){
     return ans;
 }
 
+//more optimal solution to product with out self
+vector<int> productofArrayOS(vector<int> nums){
+    int n = nums.size();
+    vector<int> ans(n,1);
+    for(int i{1};i<n;i++){
+        ans[i] = ans[i - 1] * nums[i - 1];
+    }
+    cout <<"The ans vector before adding the suffix part: " << endl;
+    for(int val: ans){
+        cout << val << endl;
+    }
+    int suffix {1};
+    for(int i{n-2};i>=0;i--){
+        suffix *= nums[i + 1];
+        ans[i] *= suffix;
+    }
+    return ans;
+}
+
 int main() {
     vector<int> nums {1,2,3,4};
-    for(int val: productofArray(nums)){
+    for(int val: productofArrayOS(nums)){
         cout<<val<<endl;
     }
-
+    // cout << "Product without self" << endl;
+    // for(int v: productExceptSelf(nums)){
+    //     cout << v << endl;
+    // }
+    //this is working perfectly well
+    //cout << productofArrayExceptSelf(nums,3) << endl;
     return 0;
 }
