47243d4739d007fee0c41e806625c04e8f2ce17b
Muhammad Atif <muhammadatifkhan.99@gamil.com>
2025-04-13 16:59:52 +0500

    reworking on pointers

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+void solve(double meal, int tip, int tax) {
+    double totalTip {0};
+    double totalTax {0};
+
+    double totalCost {0};
+    totalTax = (static_cast<double>(tax)/100);
+    totalTax = totalTax * meal;
+
+    totalTip = (static_cast<double>(meal)/100.0);
+    totalTip = totalTip * tip;
+    totalCost = meal + totalTax + totalTip;
+    // cout << totalTax << " " <<totalTip << " "<< meal << endl;
+    cout << ceil(totalCost) << endl;
+}
+
+
 int main() {
 
+    double a = 10.25;
+    int b = 17;
+    int c = 5;
+    solve(a,b,c);
+
     //pointer to a pointer
-    int a {5};
-    int *p = &a;
-    int **q = &p;
+    // int a {5};
+    // int *p = &a;
+    // int **q = &p;
 
     //NULL POINTER -> a pointer that does not point at any valid location
     int* nptr = NULL; // null pointers can not be dereferenced,as nullpointer do not point at any valid location/memory address
