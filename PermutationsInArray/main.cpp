c4565421fe8a628281d7c3beb153b56330c212b9
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-07 13:15:27 +0500

    permutation, backtracking

--- PermutationsInArray/main.cpp
+++ PermutationsInArray/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void getPerms(vector<int> &nums,int idx,vector<vector<int>> &ans) {
     if (idx==nums.size()) {
-        ans.push_back(nums);
+        ans.push_back({nums});
         return;
     }
     for (int i{0};i<nums.size();i++) {
@@ -19,8 +19,8 @@ void getPerms(vector<int> &nums,int idx,vector<vector<int>> &ans) {
 
 
 int main() {
-
-
+    vector<int> nums = {1,2,3};
+    vector<vector<int>> ans;
 
 
 
