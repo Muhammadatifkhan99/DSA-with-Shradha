35d50bd29b2111e33d0adeee808f17b8452ee1b3
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-11 23:18:31 +0500

    ABCDE pattern

---
+++ ABCDEPattern/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for (int i=1;i<n;i++) {
+        char letter = 'A';
+        for (int cnt=1;cnt <= n-i+1;cnt++) {
+            cout << letter;
+            letter ++;
+
+        }
+        letter = letter - 1;
+        for (int c = 1;c<=n-i+1;c++) {
+            cout << letter;
+            letter --;
+        }
+        cout << endl;
+    }
+
+
+
+
+    return 0;
+}
