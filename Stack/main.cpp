b52f6e6ea4fc37c23775a4d4fc33cf7d6e6475b0
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-25 10:52:31 +0500

    C++ STL implemetation of stack

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 #include<vector>
 #include<list>
+#include<stack>
 
 using namespace std;
 
-class Stack {
-    list<int> v;
+// class Stack {
+//     list<int> v;
+//
+// public:
+//     void push(int val) {
+//         v.push_front(val);
+//     }
+//
+//     void pop() {
+//         v.pop_front();
+//     }
+//
+//     int top() {
+//         return v.front(); //needs to return the actual element
+//     }
+//
+//     bool empty() {
+//         return v.size() == 0;
+//     }
+// };
 
-public:
-    void push(int val) {
-        v.push_front(val);
-    }
 
-    void pop() {
-        v.pop_front();
-    }
-
-    int top() {
-        return v.front(); //needs to return the actual element
-    }
-
-    bool empty() {
-        return v.size() == 0;
-    }
-};
 int main() {
-    Stack s;
+    stack<int> s;
 
     s.push(10);
     s.push(20);
