1a757c52cb32a1677dc71c39fd685905fd2f9174
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-27 22:23:53 +0500

    NextGreaterElement in Array using Stack

--- NextGreaterElement/main.cpp
+++ NextGreaterElement/main.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 #include<vector>
 #include<stack>
+#include <unordered_map>
 
 using namespace std;
 
 
 
 int main() {
-    vector<int> arr = {6,8,0,1,3};
-    vector<int> ng;
+    vector<int> nums1 = {4,1,2};
+    vector<int> nums2 = {1,3,4,2};
+    vector<int> ans;
 
     stack<int> s;
-
-    for (int i=arr.size()-1;i>=0;i--) {
-        //if there is no element on the stack
+    unordered_map<int,int> m;
+    for (int i=nums2.size()-1;i>=0;i--) {
+        while (s.size() > 0 && nums2[i] >= s.top()) {
+            s.pop();
+        }
         if (s.empty()) {
-            ng.push_back(-1);
-        } else if (arr[i] < s.top()) {
-            ng.push_back(s.top());
+            m[nums2[i]] = -1;
+        } else {
+            m[nums2[i]]= s.top();
         }
-        else {
-            while (!s.empty()) {
-                s.pop();
-            }
-            ng.push_back(-1);
-        }
-        s.push(arr[i]);
+        s.push(nums2[i]);
     }
-    cout << endl;
-    for (int a: ng) {
-        cout << " " << a;
+
+    for (int i=0;i<nums1.size();i++) {
+        ans.push_back(m[nums1[i]]);
+    }
+    for (int v:ans) {
+        cout << v << " " ;
     }
     return 0;
 }
