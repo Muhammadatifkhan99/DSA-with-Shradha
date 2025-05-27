96a50e74229a987ae6dd3c1f74e3e304386b72b0
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-27 23:15:44 +0500

    recursion with backtracking

--- RecusionTUF/main.cpp
+++ RecusionTUF/main.cpp
@@ -5,10 +5,41 @@ void printName(int i,int n){
     if (i>n) {
         return;
     }
-    cout << "Atif" << endl;
+    cout << "Atif " ;
     printName(i+1,n);
 }
+void printLinearly(int i,int n) {
+    if (i > n) {
+        return;
+    }
+    cout << i <<" " ;
+    printLinearly(i+1,n);
+}
+
+void printLinearBT(int n) {
+    if (n == 0) {
+        return;
+    }
+    printLinearBT(n-1);
+    cout << n << " ";
+}
+
+void printToN(int i,int n) {
+    if (i > n) {
+        return;
+    }
+    printToN(i+1,n);
+    cout << i;
+}
+
+
 int main() {
     printName(0, 4);
+    cout << "\n";
+    printLinearly(1,10);
+    cout <<"\n";
+    printLinearBT(3);
+    cout <<"\n";
+    printToN(1,4);
     return 0;
 }
