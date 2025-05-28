aab8b8ca3fcda49e50ffc21a2a27c216fafd9019
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-28 17:18:33 +0500

    functional way of doing recusion from TUF

--- RecusionTUF/main.cpp
+++ RecusionTUF/main.cpp
@@ -32,14 +32,43 @@ void printToN(int i,int n) {
     cout << i;
 }
 
+void sum1(int i,int n) {
+    if (i < 0) {
+        cout << n;
+        return;
+    }
+    sum1(i-1,n+i);
+}
+
+
+//Functional Way of doing recursion
+int sum (int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return n + sum(n-1);
+}
+
+int fact(int n) {
+    if (n == 0) return 1;
+    return n * fact(n-1);
+}
+
+
 
 int main() {
-    printName(0, 4);
-    cout << "\n";
-    printLinearly(1,10);
-    cout <<"\n";
-    printLinearBT(3);
-    cout <<"\n";
-    printToN(1,4);
+    int n = 10000;
+    cout << sum(n) << endl;
+    sum1(3,0);
+    cout << endl;
+    cout << fact(3) << endl;
+    cout << fact(4) << endl;
+    // printName(0, 4);
+    // cout << "\n";
+    // printLinearly(1,10);
+    // cout <<"\n";
+    // printLinearBT(3);
+    // cout <<"\n";
+    // printToN(1,4);
     return 0;
 }
