ef8d6e9c8466cbd61c7f27f88b3995cee4394a06
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-06-15 18:30:36 +0500

    lowest common ancestor

---
+++ LowestCommonAncestor/code.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+#include <queue>
+#include<map>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector<int> preorder){
+    idx ++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+}
+bool isIdentical(Node* p,Node* q){
+    if(p == NULL || q == NULL){
+        return p==q;
+    }
+
+    bool isLeftSame = isIdentical(p->left,q->left);
+    bool isRightSame = isIdentical(p->right,q->right);
+
+    return isLeftSame && isRightSame && p->data == q->data;
+}
+
+bool isSubTree(Node* root,Node* subRoot){
+    if(root == NULL || subRoot == NULL){
+        return root == subRoot;
+    }
+
+    if(root->data == subRoot->data && isIdentical(root,subRoot)){
+        return true;
+    }
+    return isSubTree(root->left,subRoot) || isSubTree(root->right,subRoot);
+}
+
+int ans = 0;
+int height(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftHt = height(root->left);
+    int rightHt = height(root->right);
+    ans = max(leftHt + rightHt, ans);
+    return max(leftHt,rightHt) + 1;
+}
+
+
+int Diameter(Node* root){
+    height(root);
+
+    return ans;
+}
+
+void topViewTree(Node * root){
+    queue<pair<Node*,int>> q;
+    map<int,int> m;
+
+    q.push({root,0});
+
+    while(q.size()>0){
+        Node* curr = q.front().first;
+        int currHD = q.front().second;
+        q.pop();
+
+        if(m.find(currHD) ==  m.end()){
+            m[currHD] = curr->data;
+        }
+
+        if(curr->left != NULL){
+            q.push({curr->left,currHD - 1});
+        }
+        if(curr->right != NULL){
+            q.push({curr->right,currHD + 1});
+        }
+    }
+
+    for(auto it:m){
+        cout << it.second << " ";
+    }
+    cout << endl;
+
+}
+
+void kthLevel (Node* root,int k){
+    if(root == NULL || k < 1){
+        return;
+    }
+    if(k==1){
+        cout << root->data << " ";
+        return;
+    }
+    kthLevel(root->left,k-1);
+    kthLevel(root->right,k-1);
+}
+
+Node* lowestCommonAncestor(Node *root, Node* p, Node* q){
+    if(root == NULL){
+        return NULL;
+    }
+    if(root->data == p->data && root->data == q->data){
+        return root;
+    }
+    Node* leftLCA = lowestCommonAncestor(root->left,p,q);
+    Node* rightLCA = lowestCommonAncestor(root->right,p,q);
+
+    if(leftLCA && rightLCA){
+        return root;
+    }
+    else if (leftLCA != NULL)
+    {
+        return leftLCA;
+    }
+    else {
+        return rightLCA;
+    }
+    
+}
+
+
+
+int main(){
+    // vector<int> preorder0 = {1,2,7,-1,-1,3,4,-1,-1,5,-1,-1};
+    // vector<int> preorder1 = {1,20,-1,-1,30,40,-1,-1,50,-1,-1};
+
+    // Node* r1 = buildTree(preorder0);
+    // topViewTree(r1); 
+    // kthLevel(r1,3);
+
+    cout << "Hello World" << endl;
+
+
+    return 0;
+}
