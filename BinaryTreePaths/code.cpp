b627594386945f24c6286551fb1e2ffc4d7be7e8
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-06-17 10:19:59 +0500

    binary tree paths leetcode

---
+++ BinaryTreePaths/code.cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+#include <queue>
+#include<map>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+static int idx = -1;
+Node* buildTree(vector<int> preorder){
+    idx ++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+}
+
+
+
+bool isIdentical(Node* p,Node* q){
+    if(p == NULL || q == NULL){
+        return p==q;
+    }
+
+    bool isLeftSame = isIdentical(p->left,q->left);
+    bool isRightSame = isIdentical(p->right,q->right);
+
+    return isLeftSame && isRightSame && p->data == q->data;
+}
+
+
+
+bool isSubTree(Node* root,Node* subRoot){
+    if(root == NULL || subRoot == NULL){
+        return root == subRoot;
+    }
+
+    if(root->data == subRoot->data && isIdentical(root,subRoot)){
+        return true;
+    }
+    return isSubTree(root->left,subRoot) || isSubTree(root->right,subRoot);
+}
+
+
+
+int ans = 0;
+int height(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftHt = height(root->left);
+    int rightHt = height(root->right);
+    ans = max(leftHt + rightHt, ans);
+    return max(leftHt,rightHt) + 1;
+}
+
+
+
+int Diameter(Node* root){
+    height(root);
+
+    return ans;
+}
+
+
+void topViewTree(Node * root){
+    queue<pair<Node*,int>> q;
+    map<int,int> m;
+
+    q.push({root,0});
+
+    while(q.size()>0){
+        Node* curr = q.front().first;
+        int currHD = q.front().second;
+        q.pop();
+
+        if(m.find(currHD) ==  m.end()){
+            m[currHD] = curr->data;
+        }
+
+        if(curr->left != NULL){
+            q.push({curr->left,currHD - 1});
+        }
+        if(curr->right != NULL){
+            q.push({curr->right,currHD + 1});
+        }
+    }
+
+    for(auto it:m){
+        cout << it.second << " ";
+    }
+    cout << endl;
+
+}
+
+void allPaths(Node* root,string path,vector<string> &ans){
+        if(root->left == NULL && root->right == NULL){
+            ans.push_back(path);
+            return;
+        }
+        if(root->left){
+            allPaths(root->left,path + "->"+to_string(root->left->data),ans);
+        }
+        if(root->right){
+            allPaths(root->right,path + "->"+to_string(root->right->data),ans);
+        }
+    }
+vector<string> binaryTreePaths(Node* root) {
+        vector<string> ans;
+        string path = to_string(root->data);
+
+        allPaths(root,path,ans);
+        return ans;
+    }
+
+
+
+int main(){
+    vector<int> preorder0 = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    vector<int> preorder1 = {1,20,-1,-1,30,40,-1,-1,50,-1,-1};
+
+    Node* r1 = buildTree(preorder0);
+
+    for(auto it: binaryTreePaths(r1)){
+        cout << it << endl;
+    }
+
+
+    return 0;
+}
